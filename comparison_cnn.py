afc27a3fa802a3b46c08e5858bc36f0e974e30cf
Maxi <117399104+MaxiSmplx@users.noreply.github.com>
2023-08-06 22:33:50 +0200

    Add files via upload

---
+++ comparison_cnn.py
@@ -0,0 +1,26 @@
+dogs = ["Chihuahua", "Japanese_spaniel", "Maltese_dog", "Pekinese", "Shih-Tzu", "Blenheim_spaniel", "papillon", "toy_terrier", "Rhodesian_ridgeback"
+       "Afghan_hound", "basset", "beagle", "bloodhound", "bluetick", "black-and-tan_coonhound", "Walker_hound", "English_foxhound", "redbone",
+       "borzoi", "Irish_wolfhound", "Italian_greyhound", "whippet", "Ibizan_hound", "Norwegian_elkhound", "otterhound", "Saluki", "Scottish_deerhound",
+       "Staffordshire_bullterrier", "American_Staffordshire_terrier", "Bedlington_terrier", "Border_terrier", "Kerry_blue_terrier", "Irish_terrier",
+       "Norfolk_terrier", "Norwich_terrier", "Yorkshire_terrier", "wire-haired_fox_terrier", "Lakeland_terrier", "Sealyham_terrier", "Airedale",
+       "cairn", "Australian_terrier", "Dandie_Dinmont", "Boston_bull", "giant_schnauzer", "standard_schnauzer", "Scotch_terrier", "Tibetan_terrier",
+       "silky_terrier", "soft-coated_wheaten_terrier", "West_Highland_white_terrier", "Lhasa", "flat-coated_retriever", "curly-coated_retriever", "golden_retriever",
+       "Labrador_retriever", "Chesapeake_Bay_retriever", "German_short-haired_pointer", "vizsla", "English_setter", "Irish_setter", "Gordon_setter", "Brittany_spaniel",
+       "clumber", "English_springer", "Welsh_springer_spaniel", "cocker_spaniel", "Sussex_spaniel", "Irish_water_spaniel","kuvasz", "schipperke", "groenendael", "malinois",
+       "briard", "kelpie", "komondor", "Old_English_sheepdog", "Shetland_sheepdog", "collie", "Border_collie", "Bouvier_des_Flandres", "Rottweiler", "German_shepherd",
+       "Doberman", "miniature_pinscher", "Greater_Swiss_Mountain_dog", "Bernese_mountain_dog", "Appenzeller", "EntleBucher", "boxer", "bull_mastiff", "Tibetan_mastiff",
+       "French_bulldog", "Great_Dane", "Saint_Bernard", "Eskimo_dog", "malamute", "Siberian_husky", "dalmatian", "affenpinscher", "basenji", "pug", "Leonberg", "Newfoundland",
+       "Great_Pyrenees", "Samoyed", "Pomeranian", "chow", "keeshond", "Brabancon_griffon"]
+cats = ["tabby", "tiger_cat", "Persian_cat", "Siamese_cat", "Egyptian_cat", ]
+elephants = ["Indian_elephant", "African_elephant", "tusker"]
+spiders = ["black_and_gold_garden_spider", "barn_spider", "garden_spider", "black_widow", "tarantula", "wolf_spider", ]
+cows = ["ox"]
+sheeps = ["ram", "bighorn", "ibex"]
+chickens = ["cock", "hen"]
+horses = ["sorrel"]
+squirrels = ["fox_squirrel", ]
+turtles = ["loggerhead", "leatherback_turtle", "mud_turtle", "terrapin", "box_turtle"]
+
+animals = {"dog": dogs, "cat": cats, "elephant": elephants, "spider": spiders, "cow": cows,
+           "sheep": sheeps, "chicken": chickens, "horse": horses, "squirrel": squirrels, "turtle": turtles}
+
